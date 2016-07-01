abb2e8fef18deb7de4c67ee43f20e0ddff7c09cd
Sheila <Sheila@SHEILA-PC>
2016-07-01 23:15:24 +0800

    Edited version OMG so hard

---
+++ Products.aspx
@@ -0,0 +1,73 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Customer.master" AutoEventWireup="true" CodeFile="Products.aspx.cs" Inherits="Products" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="title" Runat="Server">
+    <i class="fa fa-shopping-cart"></i> Products
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="content" Runat="Server">
+    <form runat="server" class="form-horizontal">
+        <div class="col-lg-3">
+            <div class="list-group">
+                <a href="Products.aspx" class="list-group-item active">
+                    <span class="badge">
+                        <asp:Literal ID="ltTotal" runat="server" />
+                    </span>
+                    All Categories
+                </a>
+                <asp:ListView ID="lvCategories" runat="server">
+                    <ItemTemplate>
+                        <a href='Products.aspx?c=<%# Eval("CatID") %>'
+                            class="list-group-item">
+                            <span class="badge">
+                                <%# Eval("TotalCount") %>
+                            </span>
+                            <%# Eval("Category") %>
+                        </a>
+                    </ItemTemplate>
+                </asp:ListView>
+            </div>
+        </div>
+        <div class="col-lg-9">
+            <asp:ListView ID="lvProducts" runat="server"
+                OnItemCommand="lvProducts_ItemCommand">
+                <ItemTemplate>
+                    <asp:Literal ID="ltProductID" runat="server"
+                        Text='<%# Eval("ProductID") %>' Visible="false" />
+                    <div class="col-lg-4">
+                        <a href='Details.aspx?ID=<%# Eval("ProductID") %>'
+                            style="text-decoration: none;">
+                        <div class="thumbnail">
+                        <div title='<%# Eval("Name") %>' 
+                            style="position:relative; width: 100%; height: 0; 
+                            padding-bottom: 100% ; background-repeat: no-repeat;
+                            background-position: top center; background-size: cover;
+                            background-image: url('content/img/products/<%# Eval("Image") %>');">  
+                        </div>
+                            <div class="caption">
+                                <h3 style="height: 50px;">
+                                    <%# Eval("Name") %>
+                                </h3>
+                                <h4>Category: <%# Eval("Category") %></h4>
+                                <p>
+                                    Php<%# Eval("Price", "{0: #,###,###.00}") %>
+                                </p>
+                                <asp:LinkButton ID="btnAddToCart" runat="server"
+                                    class="btn btn-block btn-success btn-lg"
+                                    CommandName="addtocart">
+                                    <i class="fa fa-plus"></i> Add to Cart
+                                </asp:LinkButton>
+                            </div>
+                        </div>
+                        </a>
+                    </div>
+                </ItemTemplate>
+                <EmptyDataTemplate>
+                    <br />
+                    <h2 class="text-center">
+                        No products found.
+                    </h2>
+                    <br />
+                </EmptyDataTemplate>
+            </asp:ListView>
+        </div>
+    </form>
+</asp:Content>
---
+++ Products.aspx.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+public partial class Products : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        // user did not select a category
+        if (Request.QueryString["c"] == null)
+        {
+            if (!IsPostBack)
+            {
+                GetCategories();
+                GetProducts();
+            }
+        }
+        else // user selected a category
+        {
+            int catID = 0;
+            bool validCategory =
+                int.TryParse(Request.QueryString["c"].ToString(),
+                    out catID);
+
+            if (validCategory)
+            {
+                GetCategories();
+                GetProducts(catID);
+            }
+            else
+                Response.Redirect("Products.aspx");
+        }
+    }
+
+    void GetCategories()
+    {
+        using (SqlConnection con = new SqlConnection(Util.GetConnection()))
+        {
+            con.Open();
+            string SQL = @"SELECT c.CatID, c.Category, 
+                (SELECT COUNT(ProductID) FROM Products
+                WHERE CatID = c.CatID) AS TotalCount 
+                FROM Categories c ORDER BY Category";
+
+            using (SqlCommand cmd = new SqlCommand(SQL, con))
+            {
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+                    lvCategories.DataSource = dr;
+                    lvCategories.DataBind();
+                }
+            }
+        }
+    }
+
+
+    void GetProducts()
+    {
+        using (SqlConnection con = new SqlConnection(Util.GetConnection()))
+        {
+            con.Open();
+            string SQL = @"SELECT p.ProductID, c.Category, 
+                p.Name, p.Image, p.Price FROM Products p
+                INNER JOIN Categories c ON p.CatID = c.CatID
+                ORDER BY NEWID()";
+
+            using (SqlCommand cmd = new SqlCommand(SQL, con))
+            {
+                using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                {
+                    DataSet ds = new DataSet();
+                    da.Fill(ds, "Products");
+
+                    lvProducts.DataSource = ds;
+                    lvProducts.DataBind();
+
+                    ltTotal.Text = ds.Tables[0].Rows.Count.ToString();
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Display products based from chosen category
+    /// </summary>
+    /// <param name="ID">Category ID</param>
+    void GetProducts(int ID)
+    {
+        using (SqlConnection con = new SqlConnection(Util.GetConnection()))
+        {
+            con.Open();
+            string SQL = @"SELECT p.ProductID, c.Category, 
+                p.Name, p.Image, p.Price FROM Products p
+                INNER JOIN Categories c ON p.CatID = c.CatID
+                WHERE p.CatID=@CatID";
+
+            using (SqlCommand cmd = new SqlCommand(SQL, con))
+            {
+                cmd.Parameters.AddWithValue("@CatID", ID);
+
+                using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                {
+                    DataSet ds = new DataSet();
+                    da.Fill(ds, "Products");
+
+                    lvProducts.DataSource = ds;
+                    lvProducts.DataBind();
+
+                }
+            }
+        }
+    }
+
+    protected void lvProducts_ItemCommand(object sender, ListViewCommandEventArgs e)
+    {
+        if (e.CommandName == "addtocart")
+        {
+            Literal ltProductID = (Literal)e.Item.FindControl("ltProductID");
+            // add to cart
+            Util.AddToCart(ltProductID.Text, "1");
+        }
+    }
+}
