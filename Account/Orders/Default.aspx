0549ee6b0ec9bd1ba72c802eb8b6802e109d79ea
Sheila <Sheila@SHEILA-PC>
2016-07-12 12:49:02 +0800

    default of orders

---
+++ Account/Orders/Default.aspx
@@ -0,0 +1,60 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Customer.master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="Account_Orders_Default" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="title" Runat="Server">
+    <i class="fa fa-shopping-cart"></i> My Orders
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="content" Runat="Server">
+    <form runat="server" class="form-horizontal">
+        <div class="col-lg-2">
+            <asp:TextBox ID="txtStart" runat="server"
+                AutoPostBack="true" OnTextChanged="SearchByDate"
+                class="form-control" type="date" />
+        </div>
+        <div class="col-lg-2">
+            <asp:TextBox ID="txtEnd" runat="server"
+                AutoPostBack="true" OnTextChanged="SearchByDate"
+                class="form-control" type="date" />
+        </div>
+        <div class="row"></div>
+        <div class="col-lg-12">
+            <table class="table table-hover">
+                <thead>
+                    <th>Order No.</th>
+                    <th>Date Ordered</th>
+                    <th>Payment Method</th>
+                    <th># of Items</th>
+                    <th>Total Amount</th>
+                    <th>Status</th>
+                    <th></th>
+                </thead>
+                <tbody>
+                    <asp:ListView ID="lvOrders" runat="server">
+                        <ItemTemplate>
+                            <tr>
+                                <td><%# Eval("OrderNo") %></td>
+                                <td><%# Eval("DateOrdered",
+                                    "{0: MMMM dd, yyyy}") %></td>
+                                <td><%# Eval("PaymentMethod") %></td>
+                                <td><%# Eval("TotalItems") %></td>
+                                <td><%# Eval("TotalAmount",
+                                    "{0: #,###,##0.00}") %></td>
+                                <td><%# Eval("Status") %></td>
+                                <td></td>
+                            </tr>
+                        </ItemTemplate>
+                        <EmptyDataTemplate>
+                            <tr>
+                                <td colspan="6">
+                                    <h2 class="text-center">
+                                        No records found.
+                                    </h2>
+                                </td>
+                            </tr>
+                        </EmptyDataTemplate>
+                    </asp:ListView>
+                </tbody>
+            </table>
+        </div>
+    </form>
+</asp:Content>
+
---
+++ Account/Orders/Default.aspx.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+public partial class Account_Orders_Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            txtStart.Attributes.Add("min",
+                DateTime.Now.AddMonths(-3).ToString("yyyy-MM-dd"));
+            GetOrders();
+        }
+    }
+
+    void GetOrders()
+    {
+        using (SqlConnection con = new SqlConnection(Util.GetConnection()))
+        {
+            con.Open();
+            string SQL = @"SELECT DISTINCT o.OrderNo, o.DateOrdered,
+                o.PaymentMethod, o.Status,
+                (SELECT SUM(Quantity) FROM OrderDetails
+                WHERE OrderNo = o.OrderNo) AS TotalItems, 
+                (SELECT SUM(Amount) FROM OrderDetails
+                WHERE OrderNo = o.OrderNo) * 1.05 AS TotalAmount
+                FROM Orders o INNER JOIN OrderDetails od
+                ON o.OrderNo = od.OrderNo
+                WHERE od.UserID=@UserID
+                ORDER BY o.DateOrdered DESC";
+
+            using (SqlCommand cmd = new SqlCommand(SQL, con))
+            {
+                cmd.Parameters.AddWithValue("@UserID",
+                    Session["userid"].ToString());
+
+                using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                {
+                    DataSet ds = new DataSet();
+                    da.Fill(ds, "ikawnabahalamaglagayngvaluesdito");
+
+                    lvOrders.DataSource = ds;
+                    lvOrders.DataBind();
+                }
+            }
+        }
+    }
+
+    void GetOrders(DateTime start, DateTime end)
+    {
+        using (SqlConnection con = new SqlConnection(Util.GetConnection()))
+        {
+            con.Open();
+            string SQL = @"SELECT DISTINCT o.OrderNo, o.DateOrdered,
+                o.PaymentMethod, o.Status,
+                (SELECT SUM(Quantity) FROM OrderDetails
+                WHERE OrderNo = o.OrderNo) AS TotalItems, 
+                (SELECT SUM(Amount) FROM OrderDetails
+                WHERE OrderNo = o.OrderNo) * 1.05 AS TotalAmount
+                FROM Orders o INNER JOIN OrderDetails od
+                ON o.OrderNo = od.OrderNo
+                WHERE od.UserID=@UserID AND
+                (o.DateOrdered BETWEEN @start AND @end)
+                ORDER BY o.DateOrdered DESC";
+
+            using (SqlCommand cmd = new SqlCommand(SQL, con))
+            {
+                cmd.Parameters.AddWithValue("@UserID",
+                    Session["userid"].ToString());
+                cmd.Parameters.AddWithValue("@start", start);
+                cmd.Parameters.AddWithValue("@end", 
+                    end.AddDays(1).AddSeconds(-1));
+
+                using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                {
+                    DataSet ds = new DataSet();
+                    da.Fill(ds, "ikawnabahalamaglagayngvaluesdito");
+
+                    lvOrders.DataSource = ds;
+                    lvOrders.DataBind();
+                }
+            }
+        }
+    }
+
+    protected void SearchByDate(object sender, EventArgs e)
+    {
+        DateTime start = DateTime.Now;
+        DateTime end = DateTime.Now;
+        bool validStart = DateTime.TryParse(txtStart.Text, out start);
+        bool validEnd = DateTime.TryParse(txtEnd.Text, out end);
+
+        if (validStart && validEnd)
+        {
+            //search orders by date range
+            GetOrders(start, end);
+        }
+    }
+}
